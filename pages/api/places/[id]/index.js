940cb2fc159285011c6e833f1647406820078513
Predrag <predrag.babic@tactix-sports.com>
2024-09-20 10:11:44 +0200

    install dbConnect and models

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,20 @@
-import { places } from "../../../../lib/db.js";
+// import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/models/Connect";
+import Places from "@/db/models/Places";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  if (!id) {
-    return;
+  if (request.method === "GET") {
+    try {
+      const place = await Places.findById(id);
+      if (!place) {
+        return response.status(404).json({ status: "Not found" });
+      }
+      return response.status(200).json(place);
+    } catch (error) {
+      return res.status(500).json({ error: "Error fetching place" });
+    }
   }
-
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
-  }
-
-  response.status(200).json(place);
 }
